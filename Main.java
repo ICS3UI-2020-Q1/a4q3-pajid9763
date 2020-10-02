5ad4b23a6eaeb158a4265d29b99d21361a4725d8
pajid9763 <pajid9763@wrdsb.ca>
2020-10-02 18:08:09 +0000

    finished sum of range program

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * program that calculates sum of range of numbers
+ * @author Darian
  */
 public class Main {
 
@@ -9,7 +10,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+  Scanner input = new Scanner(System.in);
+
+  // create a variable to keep track of what number we are on
+  int count = 1;
+  // the accumulator to add the count into
+  int total = 0;
+
+  // ask the user for a positive integer
+  System.out.println("Please enter an integer to add up the numbers inbetween");
+  int number = input.nextInt();
+
+  while (count <= number){
+    // add the numbers as we go into total 
+    total = total + count;
+    // add 1 to the count to increase it 
+    count = count + 1; 
+  }
+    // print the sum of the range of numbers
+    System.out.println("The sum from 1 - " + number + " is " + total);
   }
 }
